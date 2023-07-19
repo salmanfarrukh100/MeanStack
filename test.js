1d2155e6e5bc1247756d0941dc59dbddb3d2d437
Salman Farrukh <salmanfarrukh100@hotmail.com>
2023-07-19 16:50:25 +0500

    added js anf html file

---
+++ test.js
@@ -0,0 +1,13 @@
+
+console.log(`hello JS ES ${10*10}`);
+var hello = "salman farrukh"
+const hello2 = "yasir hameed"
+
+let hello3 = "usman farrukh"
+
+function Calc(num1, num2)
+{
+    return num1+num2;
+}
+
+const hello5 = (num1,num2)=> num1+num2; //es6 fat arrows
